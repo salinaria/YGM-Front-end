6908ad1a0b5d25bf3d56bc0b29ef0f7cb5541f7e
aliaa80 <a.ansari80@outlook.com>
2021-12-05 22:15:54 +0330

    Develop navigation bar it could be seen in /movie

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import classes from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
+import Home from "../../assets/Home.svg";
+import Search from "../../assets/Search.svg";
+import Logo from "../../assets/Logo.svg";
+import Library from "../../assets/Bookmark.svg";
+import Profile from "../../assets/Profile.svg";
+
+const Movie = (props) => {
+  return (
+    <div className={classes.body}>
+      <NavLink
+        className={classes.item}
+        to={"/"}
+        style={{ display: "flex", flexDirection: "row" }}
+      >
+        <div>
+          <img className={classes.NavItemImage} src={Home} alt="home" />
+          <p className={classes.NavItemText}>Home</p>
+        </div>
+      </NavLink>
+      <NavLink
+        className={classes.item}
+        to={"/"}
+        style={{ display: "flex", flexDirection: "row" }}
+      >
+        <div>
+          <img className={classes.NavItemImage} src={Search} alt="home" />
+          <p className={classes.NavItemText}>Search</p>
+        </div>
+      </NavLink>
+
+      <img className={classes.logo} src={Logo} alt="logo" />
+
+      <NavLink
+        className={classes.item}
+        to={"/"}
+        style={{ display: "flex", flexDirection: "row" }}
+      >
+        <div>
+          <img className={classes.NavItemImage} src={Library} alt="home" />
+          <p className={classes.NavItemText}>Library</p>
+        </div>
+      </NavLink>
+
+      <NavLink
+        className={classes.item}
+        to={"/"}
+        style={{ display: "flex", flexDirection: "row" }}
+      >
+        <div>
+          <img className={classes.NavItemImage} src={Profile} alt="home" />
+          <p className={classes.NavItemText}>Profile</p>
+        </div>
+      </NavLink>
+    </div>
+  );
+};
+
+export default Movie;
--- src/components/moviePage/Movie.js
+++ src/components/moviePage/Movie.js
@@ -1,8 +1,14 @@
 import React from "react";
 import classes from "./Movie.module.css";
+import Navbar from "../Navbar/Navbar"
 
 const Movie = (props) => {
-  return <div>hello</div>;
+  return (
+    <div>
+      <Navbar/>
+      <p>hello</p>
+    </div>
+  );
 };
 
 export default Movie;
